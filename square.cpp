270c4a091a7f2c50a1cabe80473bd09f874db488
DudeSoPErfect <72893727+DudeSoPErfect@users.noreply.github.com>
2021-08-29 13:40:23 +0300

    Update square.cpp

--- square.cpp
+++ square.cpp
@@ -7,30 +7,15 @@
 #include <stdbool.h>
 #include <assert.h>
 #include "square.h"
+#include "testing.h"
 
 const double EPS = 1e-8;
-
-/*
-    \brief Функция принимает значение и возвращает булевое значение,
-    является ли правдой, что значение равно 0.
-    \param[in] u - исходное значение.
- */
 
 bool is_zero(float u) {
     assert(!isnan(u));
     return fabs(u) < EPS;
 
 }
-
-/*
-    \brief Функция для решения частного случая квадратных уравнений, а именно: линейных.
-    \details Принимает на вход два коэффициента исходного уравнения и указатель на его корень.
-    Возвращает единственное из значений "-1", "0" и "1", что соответственно означает количество
-    корней исходного уравнения - "бесконечность", "ноль" или "один".
-    При наличии корня, изменяет соответствующую переменную по указателю на значение вышеупомянутого корня.
-    \param[out] x_1 - указатель на корень.
-    \param[in] a, b - коэффициенты.
- */
 
 int solving_the_line(float a, float b, float *x_1) {
 
@@ -52,23 +37,11 @@ int solving_the_line(float a, float b, float *x_1) {
 
 }
 
-/*
-    \brief Функция для решения квадратного уравнения.
-    \details Принимает на вход три коэффициента исходного уравнения и два указателя на его корни.
-    Возвращает единственное из значений "0", "1" и "2", что соответственно означает количество
-    корней исходнодного уравнения - "один", "два" или "три".
-    При наличия корня/корней, изменяет соответствующую/соответствующие переменную/переменные
-    по указателю на значение/значения вышеупомянутого корня/корней.
-    \param[out] x_1, x_2 - указатели на корни.
-    \param[in] a, b, c - коэффициенты.
- */
-
 int solving_the_square(float a, float b, float c, float *x_1, float *x_2) {
 
-    assert(!(isnan(a) || isnan(b) || isnan(c)));
+    assert((isfinite(a) || isfinite(b) || isfinite(c)));
     assert(x_1 != NULL);
     assert(x_2 != NULL);
-    assert(!(isinf(a) || isinf(b) || isinf(c)));
     assert(x_1 != x_2);
 
     float D = b * b - 4 * a * c;
@@ -84,7 +57,6 @@ int solving_the_square(float a, float b, float c, float *x_1, float *x_2) {
             *x_2 = (b * (-1) + sqrt(D)) / (2 * a);
             return SQUARE_TWO_ROOTS;
         } else {
-            //printf("No roots\n");
             *x_1 = 0;
             return SQUARE_ZERO_ROOTS;
         }
@@ -92,14 +64,6 @@ int solving_the_square(float a, float b, float c, float *x_1, float *x_2) {
 
 
 }
-
-/*
-    \brief Функция для вывода количества корней и их значений.
-    \details Принимает на вход количество корней и их значения.
-    Выводит на экран количество корня/корней и его/их значение, в зависимости от их количества.
-    \param[in] num_of_roots - количество корней. x_1, x_2 - значения корней
-
- */
 
 int printing(int num_of_roots, float x_1, float x_2) {
 
