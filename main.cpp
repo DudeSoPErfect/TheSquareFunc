df5868ac9738a66375a5079ded57ccfd24631f2b
DudeSoPErfect <72893727+DudeSoPErfect@users.noreply.github.com>
2021-08-29 14:10:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,23 @@
 #include <stdbool.h>
 #include <assert.h>
 #include "square.h"
-void testing();
+#include "testing.h"
 
 int main() {
     float a = 0, b = 0, c = 0, x_1 = 0, x_2 = 0;
     testing();
 
-    scanf("%f %f %f", &a, &b, &c);
-    int nroots = solving_the_square(a, b, c, &x_1, &x_2);
-    printing(nroots, x_1, x_2);
+    if (3 == scanf("%f %f %f", &a, &b, &c))
+    {
+        int nroots = solving_the_square(a, b, c, &x_1, &x_2);
+        printing(nroots, x_1, x_2);
 
-    return 0;
+        return 0;
+    }
+    else
+    {
+        printf("Wrong input data");
+        return 0;
+    }
 }
+
