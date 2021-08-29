76cde10f24ba9d23c2dbea12257816bc1328a2ff
DudeSoPErfect <72893727+DudeSoPErfect@users.noreply.github.com>
2021-08-29 13:39:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,11 @@
 #include <stdbool.h>
 #include <assert.h>
 #include "square.h"
-void placing_the_vars(int num_of_roots);
-
-
+void testing();
 
 int main() {
     float a = 0, b = 0, c = 0, x_1 = 0, x_2 = 0;
-    int num_of_roots = 0;
-    placing_the_vars(num_of_roots);
+    testing();
 
     scanf("%f %f %f", &a, &b, &c);
     int nroots = solving_the_square(a, b, c, &x_1, &x_2);
@@ -19,4 +16,3 @@ int main() {
 
     return 0;
 }
-
